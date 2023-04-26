b4eaffc49dc7fcab1e13ea933ded82145726d422
LudwigEffort <dr.feynman65@gmail.com>
2023-04-26 16:58:36 +0200

    variables done

--- BroCode/c_sharp/variables/Program.cs
+++ BroCode/c_sharp/variables/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Type Casting
+
+double a = 3.14;
+int b = Convert.ToInt32(a);
+
+int c = 123;
+double d = Convert.ToDouble(c) + 0.5;
+
+int e = 321;
+string f = Convert.ToString(e);
+
+string g = "$";
+char h = Convert.ToChar(g);
+
+string i = "true";
+bool j = Convert.ToBoolean(i);
+
+Console.WriteLine(a);
+Console.WriteLine(a.GetType());
+
+Console.WriteLine(b);
+Console.WriteLine(b.GetType());
+
+Console.WriteLine(d);
+
+Console.WriteLine(f);
+Console.WriteLine(f.GetType());
+
+Console.WriteLine(h);
+Console.WriteLine(h.GetType());
+
+Console.WriteLine(j);
+Console.WriteLine(j.GetType());
