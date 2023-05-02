770f07bf74e662d8b8f0fba6d3fd382a63de0d54
LudwigEffort <dr.feynman65@gmail.com>
2023-05-02 18:59:43 +0200

    done logical operators

--- BroCode/c_sharp/logical_operators/Program.cs
+++ BroCode/c_sharp/logical_operators/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//? logical operators = Can be used to check if more than 1 condition is true/false
+
+//? && (AND)
+//? || (OR)
+
+Console.WriteLine("What's the temperature outside: (C)");
+double temp = Convert.ToDouble(Console.ReadLine());
+
+if (temp >= 10 && temp <= 25)
+{
+    Console.WriteLine("It's warm outside!");
+}
+else if (temp <= -50 || temp >= 50)
+{
+    Console.WriteLine("DON'T GO OUTSIDE!");
+}
+
+Console.ReadKey();
