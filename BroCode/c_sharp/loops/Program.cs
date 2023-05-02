fb892adcd092047819e78cc718f49b8347c2d4ad
LudwigEffort <dr.feynman65@gmail.com>
2023-05-02 20:00:28 +0200

    loops done

--- BroCode/c_sharp/loops/Program.cs
+++ BroCode/c_sharp/loops/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//? while loop = repeats some code while some condition remains true
+
+/*
+string name = "";
+
+while (name == "")
+{
+    Console.WriteLine("Enter your name: ");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine("Hello " + name);
+*/
+
+//? for loop = repeats some code a FINITE amount of time
+
+/*
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+
+//? nested loops = loops inside of other loops. Uses vary. Used a lot in sorting algorithms.
+
+Console.WriteLine("How many rows?: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many columns?: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What symbol?: ");
+string symbol = Console.ReadLine();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(symbol);
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
