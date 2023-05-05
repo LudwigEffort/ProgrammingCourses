74972e96c034f6df7f96733669e16ab3dc1f0705
LudwigEffort <dr.feynman65@gmail.com>
2023-05-05 16:02:42 +0200

    arrays done

--- BroCode/c_sharp/arrays/Program.cs
+++ BroCode/c_sharp/arrays/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//? array = a varaible that can store multiple values. Fixed size
+
+/*
+string[] cars = { "BMW", "Mustang", "Corvette" };
+
+cars[0] = "fiat";
+
+for (int i = 0; i < cars.Length; i++)
+{
+    Console.WriteLine(cars[i]);
+}
+*/
+
+//? foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+/*
+string[] cars = { "BMW", "Mustang", "Corvette" };
+
+cars[0] = "fiat";
+
+foreach (string car in cars)
+{
+    Console.WriteLine(car);
+}
+*/
+
