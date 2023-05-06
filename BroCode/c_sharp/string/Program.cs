820f89db759536cee4bf8b3ad25a5048ebf52932
LudwigEffort <dr.feynman65@gmail.com>
2023-05-06 22:50:45 +0200

    add string interpolation in string

--- BroCode/c_sharp/string/Program.cs
+++ BroCode/c_sharp/string/Program.cs
@@ -1,4 +1,5 @@
-﻿string fullName = "Bro code";
+﻿/*
+string fullName = "Bro code";
 string phoneNumber = "123-456-789";
 
 //fullName = fullName.ToUpper();
@@ -16,3 +17,20 @@ Console.WriteLine(userName);
 Console.WriteLine(fullName.Length);
 Console.WriteLine(firstName);
 Console.WriteLine(lastName);
+*/
+
+//? string interpolation = allows us to insert variables into a string litteral
+//?                        precede a string literal with $
+//?                        {} are placeholders
+
+string firstName = "Ludwig";
+string lastName = "Effort";
+int age = 25;
+
+/*
+Console.WriteLine("Hello " + firstName + "" + lastName + "."); //? classic way
+Console.WriteLine("You are " + age + " years old.");
+*/
+
+Console.WriteLine($"Hello {firstName} {lastName}");
+Console.WriteLine($"You are {age} years old.");
