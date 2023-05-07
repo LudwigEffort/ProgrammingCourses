6bc2a92dfb9295981a4f6e09a2159806872b8fb2
LudwigEffort <dr.feynman65@gmail.com>
2023-05-07 19:12:15 +0200

    classes done

---
+++ BroCode/c_sharp/classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+using System.Net.Http;
+using System;
+
+namespace classes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //? class = A bundle of related code.
+            //?       = Can be used as a blueprint to create objects (OOP)
+
+            Message.Hello();
+            Message.Waiting();
+            Message.Bye();
+        }
+
+    }
+}
