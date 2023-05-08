0fe7828b37640637e8b9496f19862db632efad46
LudwigEffort <dr.feynman65@gmail.com>
2023-05-08 22:58:12 +0200

    abjects as arguments done

---
+++ BroCode/c_sharp/objects_as_arguments/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace objects_as_arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mustang", "red");
+
+            Car car2 = Copy(car1);
+
+            ChangeColor(car1, "silver");
+
+            Console.WriteLine(car1.color + " " + car1.model);
+            Console.WriteLine(car2.color + " " + car2.model);
+
+            Console.ReadKey();
+        }
+
+        public static void ChangeColor(Car car, string color)
+        {
+            car.color = color;
+        }
+
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
+    }
+    class Car
+    {
+        public string model;
+        public string color;
+
+        public Car(string model, string color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
