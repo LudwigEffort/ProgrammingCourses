4d51a614b1bca8113b1125c019d455befd383f07
LudwigEffort <dr.feynman65@gmail.com>
2023-05-08 22:23:19 +0200

    abstract classses and array of objects done

---
+++ BroCode/c_sharp/abstract_classes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace abstract_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //? abstract classes = modifier that indicates missing components or incomplete implementation
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
+
+            Console.WriteLine(bicycle.speed);
+            Console.WriteLine(bicycle.wheels);
+            bicycle.go();
+
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            boat.go();
+
+            Console.ReadKey();
+        }
+
+        abstract class Vehicle
+        {
+            public int speed = 0;
+
+            public void go()
+            {
+                Console.WriteLine("This vehicle is moving!");
+            }
+        }
+
+        class Car : Vehicle
+        {
+            public int wheels = 4;
+        }
+
+        class Bicycle : Vehicle
+        {
+            public int wheels = 2;
+        }
+        class Boat : Vehicle
+        {
+            public int wheels = 0;
+        }
+    }
+}
---
+++ BroCode/c_sharp/array_of_objects/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace abstract_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Car[] garage = new Car[3];
+
+            Car car1 = new Car("Mustang");
+            Car car2 = new Car("Corvette");
+            Car car3 = new Car("Lambo");
+
+            garage[0] = car1;
+            garage[1] = car2;
+            garage[2] = car3;
+            */
+
+            Car[] garage = { new Car("Mustang"), new Car("Corvette"), new Car("Lambo") }; //? Simple method
+
+            foreach (Car car in garage)
+            {
+                Console.WriteLine(car.model);
+            }
+
+            Console.ReadKey();
+        }
+
+        class Car
+        {
+            public string model;
+
+            public Car(string model)
+            {
+                this.model = model;
+            }
+        }
+
+    }
+}
+
