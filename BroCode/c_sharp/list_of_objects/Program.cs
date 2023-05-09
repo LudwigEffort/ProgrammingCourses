dc5eea144213f99cb4c385f049c9d38bcc08283c
LudwigEffort <dr.feynman65@gmail.com>
2023-05-09 22:37:34 +0200

    list of objects done

---
+++ BroCode/c_sharp/list_of_objects/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace list_of_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            Player player1 = new Player("Chad");
+            Player player2 = new Player("Steve");
+            Player player3 = new Player("Karen");
+
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(player3);
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.WriteLine("--------------------");
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player.username);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public string username;
+        public Player(string username)
+        {
+            this.username = username;
+        }
+        public override string ToString()
+        {
+            return "The username is: " + username;
+        }
+    }
+}
