e79e4e2167932be5738f4dcf10693bf98fe738c1
LudwigEffort <dr.feynman65@gmail.com>
2023-05-09 16:26:35 +0200

    ToString method done

---
+++ BroCode/c_sharp/ToString_method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ToString_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //? ToString() = converts an objects to its string representation so taht it is suitable for display
+
+            Car car = new Car("Chevy", "Corvette", 2022, "Blue");
+
+            Console.WriteLine(car);
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        string make;
+        string model;
+        int year;
+        string color;
+
+        public Car(string make, string model, int year, string color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+        public override string ToString()
+        {
+            //string message = "This ia a " + make + " " + model;
+            //return message;
+            return "This ia a " + make + " " + model;
+        }
+    }
+}
