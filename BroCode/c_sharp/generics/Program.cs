0c8bfb569eb84e200056c078c476b1b1762c0b92
LudwigEffort <dr.feynman65@gmail.com>
2023-05-10 13:33:33 +0200

    generics done

---
+++ BroCode/c_sharp/generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //? generic = "not specific to a particular data type"
+            //?           add <T> to: classes, methods, fields, etc.
+            //?           allows for code reusability for different data types
+
+            int[] intArray = { 1, 2, 3 };
+            double[] doubleArray = { 1.0, 2.0, 3.0 };
+            string[] stringArray = { "1", "2", "3" };
+
+            displayElement(intArray);
+            displayElement(doubleArray);
+            displayElement(stringArray);
+
+            Console.ReadKey();
+        }
+        public static void displayElement<T>(T[] array)
+        {
+            foreach (T item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
