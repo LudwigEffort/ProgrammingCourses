bc5de7c250bc60ada0c799771b80df0d4b832430
LudwigEffort <dr.feynman65@gmail.com>
2023-06-05 11:45:19 +0200

    add insert query for category

---
+++ free_code_camp/asp_net_courses/asp_net_mvc_dotnet6_course/BulkyBook/BulkyBook Local.session.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT Categories ON
+INSERT INTO Categories (Id, Name, DisplayOrder, CreatedDateTime)
+VALUES (4, 'Math', 4, '2023-06-05 10:00:00')
+SET IDENTITY_INSERT Categories OFF;
