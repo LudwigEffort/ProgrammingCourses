ae25bfb68619209eefba99c57281d9995c0c87cf
LudwigEffort <dr.feynman65@gmail.com>
2023-06-09 13:42:36 +0200

    add delete category

--- free_code_camp/asp_net_courses/asp_net_mvc_dotnet6_course/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ free_code_camp/asp_net_courses/asp_net_mvc_dotnet6_course/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -84,6 +84,36 @@ public class CategoryController : Controller
         return View(obj);
     }
 
+    //? GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    //? POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
