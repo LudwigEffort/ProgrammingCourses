65582e89fb3e6a4cc084a447f190fdc8b7cf1d3b
LudwigEffort <dr.feynman65@gmail.com>
2023-06-17 20:06:22 +0200

    implement additional methods (upsert and delete) and add error controller

--- free_code_camp/asp_net_courses/industry_rest_api_dotnet6_course/BuberBreakfast/Controllers/BreakfastsController.cs
+++ free_code_camp/asp_net_courses/industry_rest_api_dotnet6_course/BuberBreakfast/Controllers/BreakfastsController.cs
@@ -73,12 +73,26 @@ public class BreakfastsController : Controller
     [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
-        return Ok(request);
+        var breakfast = new Breakfast(
+                    id,
+                    request.Name,
+                    request.Description,
+                    request.StartDataTime,
+                    request.EndDataTime,
+                    DateTime.UtcNow,
+                    request.Savory,
+                    request.Sweet);
+
+        _breakfastService.UpsertBreakfast(breakfast);
+
+        // TODO: return 201 if a new breakfast was created
+        return NoContent();
     }
 
     [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id)
     {
-        return Ok(id);
+        _breakfastService.DeleteBreakfast(id);
+        return NoContent();
     }
 }
--- free_code_camp/asp_net_courses/industry_rest_api_dotnet6_course/BuberBreakfast/Services/Breakfast/IBreakfastService.cs
+++ free_code_camp/asp_net_courses/industry_rest_api_dotnet6_course/BuberBreakfast/Services/Breakfast/IBreakfastService.cs
@@ -3,5 +3,7 @@ using BuberBreakfast.Models;
 public interface IBreakfastService
 {
     void CreateBreakfast(Breakfast breakfast);
+    void DeleteBreakfast(Guid id);
     Breakfast GetBreakfast(Guid id);
+    void UpsertBreakfast(Breakfast breakfast);
 }
